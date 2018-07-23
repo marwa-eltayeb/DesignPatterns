b3639a428a9cd263a76020bae890343506351423
Marwa-Said <marwa_eltayeb@yahoo.com>
2018-07-23 20:18:24 +0200

    Design Patterns

---
+++ src/designpattern/Creational_Patterns/Singleton.java
@@ -0,0 +1,29 @@
+package designpattern.Creational_Patterns;
+
+/**
+ * @author Marwa
+ */
+public class Singleton {  // lazy construction
+    
+    // the class variable is null if no instance is instantiated
+    private static Singleton instance = null;
+
+
+    private Singleton() {
+    
+    }
+    
+    // lazy construction of the instance
+   public static Singleton getInstance() {
+       
+     if (instance == null) {
+     instance = new Singleton();
+     }
+   
+     return instance;
+   
+   }
+    
+    
+
+}
